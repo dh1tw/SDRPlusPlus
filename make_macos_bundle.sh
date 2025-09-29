8e7c700fa15f9b15c17e6981fd666339c652a5f3
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2025-09-30 00:06:10 +0200

    scaffolding for the midi plugin

--- make_macos_bundle.sh
+++ make_macos_bundle.sh
@@ -69,6 +69,7 @@ bundle_install_binary $BUNDLE $BUNDLE/Contents/Plugins $BUILD_DIR/misc_modules/r
 bundle_install_binary $BUNDLE $BUNDLE/Contents/Plugins $BUILD_DIR/misc_modules/rigctl_client/rigctl_client.dylib
 bundle_install_binary $BUNDLE $BUNDLE/Contents/Plugins $BUILD_DIR/misc_modules/rigctl_server/rigctl_server.dylib
 bundle_install_binary $BUNDLE $BUNDLE/Contents/Plugins $BUILD_DIR/misc_modules/scanner/scanner.dylib
+bundle_install_binary $BUNDLE $BUNDLE/Contents/Plugins $BUILD_DIR/misc_modules/midi_ctl/midi_ctl.dylib
 
 # ========================= Finalize =========================
 
